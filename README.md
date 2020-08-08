0ab6b3b3d5e4d82921ffb249778a0828202d5fd6
pablunsky <hpablo677@gmail.com>
2020-08-07 23:36:10 -0600

    README and app

---
+++ README.md
@@ -0,0 +1,22 @@
+## Pablo Andres Hernandez Rivera
+## 201603191
+# Software Avanzado: Tarea practica 1
+El siguiente repositorio contiene la realizacion de la tarea practica 1: Un cliente web que consume una API que provee acceso y publicacion de contactos.
+## Uso
+Para utilizar el cliente, debe descargar el proyecto:
+```
+git init
+git pull https://github.com/pablunsky/sa-tp1.git
+```
+Proceda a instalar las dependencias de node:
+```
+npm install
+```
+Para ejecutar la aplicacion, corra el siguiente comando:
+```
+node index.js
+```
+Si se dirige al navegador, en la ruta http://localhost:3500/ debera visualizar la pagina principal.
+Al hacer clic en el boton "Insertar" publicara un nuevo contacto con el nombre "201603191".
+Si se dirige a la pestana "Listar" podra ver todos los contactos agregados con el nombre "201603191".
+Esto se logra con el consumo del web service disponible en https://api.softwareavanzado.world/index.php
---
+++ index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+const bodyParser = require('body-parser')
+var app = express();
+var path = require('path');
+request = require('request');
+
+//Middlewears utiles para trabajar con express
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'))
+
+//Sirve la pagina en el folder public para la ruta inicial
+app.get('/', function (req, res)
+{
+    res.send("APLICACION INICIADA");
+});
+
+//Endpoint que se encarga de realizar un POST de un contacto
+app.get('/postData', function (req, res)
+{
+    //Utiliza la libreria request de node para facilitar las peticiones HTTP
+    request(
+        {
+            method: 'POST',
+            uri: 'https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=hal',
+            headers: [
+                { 'Content-Type': 'application/json' }
+            ],
+            body: JSON.stringify({ name: '201603191' })
+        }, function (error, response, body)
+    {
+        if (response.statusCode == 201)
+        {
+            //Si la peticion es exitosa, el servidor es informado
+            console.log('Contacto publicado');
+        } else
+        {
+            //Manejo de error
+            console.log('Error: ' + response.statusCode);
+            console.log(body);
+        }
+    }).pipe(res); //Retorna la respuesta del Web Service
+});
+
+app.get('/getPosted', function (req, res)
+{
+    //Realiza una peticion al endpoint de consulta para el parametro de busqueda que incluye 201603191 en el nombre
+    request({
+        uri: 'https://api.softwareavanzado.world/index.php?option=com_contact&webserviceVersion=1.0.0&webserviceClient=administrator&filter[search]=201603191&api=Hal',
+    }).pipe(res); //Retorna la respuesta del Web Service
+});
+
+const server = app.listen(3500, function ()
+{
+    console.log('Aplicacion inicializada');
+});
