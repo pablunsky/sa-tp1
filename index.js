86fecfcf0372429b7ee1aa99d7e7e69075c1dfda
pablunsky <hpablo677@gmail.com>
2020-08-08 10:30:49 -0600

    Authorization

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express');
 const bodyParser = require('body-parser')
 var app = express();
 var path = require('path');
-request = require('request');
+var request = require('request');
+
+//Acceso a archivo de variables de entorno
+const dotenv = require('dotenv');
+dotenv.config();
 
 //Middlewears utiles para trabajar con express
 app.use(bodyParser.json());
@@ -21,11 +25,14 @@ app.get('/postData', function (req, res)
     request(
         {
             method: 'POST',
-            uri: 'https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=hal',
+            uri: `https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=hal`,
             headers: [
-                { 'Content-Type': 'application/json' }
+                {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${process.env.CLIENT_SECRET}`
+                }
             ],
-            body: JSON.stringify({ name: '201603191' })
+            body: JSON.stringify({ grant_type: 'client_credentials', client_id: 'sa', client_secret: `${process.env.CLIENT_SECRET}`, name: '201603191' })
         }, function (error, response, body)
     {
         if (response.statusCode == 201)
@@ -43,10 +50,17 @@ app.get('/postData', function (req, res)
 
 app.get('/getPosted', function (req, res)
 {
-    //Realiza una peticion al endpoint de consulta para el parametro de busqueda que incluye 201603191 en el nombre
-    request({
-        uri: 'https://api.softwareavanzado.world/index.php?option=com_contact&webserviceVersion=1.0.0&webserviceClient=administrator&filter[search]=201603191&api=Hal',
-    }).pipe(res); //Retorna la respuesta del Web Service
+    //Configuracion de la peticion, al endpoint que obtiene contactos con 201603191 en el nombre y con el header de autorizacion
+    let options = {
+        method: 'GET',
+        url: 'https://api.softwareavanzado.world/index.php?option=com_contact&webserviceVersion=1.0.0&webserviceClient=administrator&filter[search]=201603191&api=Hal',
+        headers: {
+            'content-type': 'application/json',
+            'authorization': `Bearer ${process.env.CLIENT_SECRET}`
+        }
+    }
+    request(options).pipe(res); //Retorna la respuesta del Web Service
+
 });
 
 //Inicializa la aplicacion en el puerto 3500
